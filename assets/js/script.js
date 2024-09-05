59d04254aee87ca39b53e4fab721dabc5455e563
Alexander_L <alexander.w.leyva@gmail.com>
2024-09-04 23:14:18 -0500

    added date picker and handleAddTask Function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 // Retrieve tasks and nextId from localStorage
-let taskList = JSON.parse(localStorage.getItem("tasks"));
+let taskList = JSON.parse(localStorage.getItem("tasks")) || [];
 let nextId = JSON.parse(localStorage.getItem("nextId"));
-
+const taskForm = $('#addTaskBtn')
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
@@ -13,14 +13,33 @@ function createTaskCard(task) {
 
 }
 
-// Todo: create a function to render the task list and make cards draggable
+ /// Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
 
 }
 
-// Todo: create a function to handle adding a new task
-function handleAddTask(event){
+// // Todo: create a function to handle adding a new task
+function handleAddTask(){
+    const taskTitleInput = $('#taskTitleInput').val();
+    const taskDateInput = $('#taskDateInput').val();
+    const taskDescripInput = $('#taskDescripInput').val();
 
+    if(!taskTitleInput || !taskDateInput || !taskDescripInput) {
+        alert('Please fill out the form.')
+        return;
+    }
+    else {
+        const newTask = {
+            taskTitle: taskTitleInput,
+            taskDate: taskDateInput,
+            taskDescription: taskDescripInput,
+            status: 'to-do'
+        };
+        
+        taskList.push(newTask);
+
+        localStorage.setItem('tasks', JSON.stringify(taskList))
+    }
 }
 
 // Todo: create a function to handle deleting a task
@@ -33,7 +52,21 @@ function handleDrop(event, ui) {
 
 }
 
+taskForm.on('click', handleAddTask)
+
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
+    renderTaskList()
 
+    $('#taskDateInput').datepicker({
+        changeMonth: true,
+        changeYear: true
+    })
+
+    $('.lane').droppable({
+        accept: '.draggable',
+        drop: handleDrop
+    })
 });
+
+
